a40fd33684a47867e09664d381c4fcb5768709e5
Jeoganni Canda <jeo.canda@mlhuillier.com>
2022-03-14 23:23:15 +0800

    Add files via upload

---
+++ KioskWebService/Models/FLACounterModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KioskWebService.Models
+{
+    public class FLACounterModel
+    {
+        public int id { get; set; }
+        public string branchCode { get; set; }
+        public int zoneCode { get; set; }
+        public string operatorID { get; set; }
+        public int counterNum { get; set; }
+        public DateTime syscreated { get; set; }
+        public int respCode { get; set; }
+        public string respMessage { get; set; }
+    }
+}
